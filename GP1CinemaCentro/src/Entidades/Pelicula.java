797cbc1094c8a8f43149a7f5161930c230facafe
guada <bguadalupemilagros@gmail.com>
2025-10-23 15:41:16 -0300

    clase pelicula

---
+++ GP1CinemaCentro/src/Entidades/Pelicula.java
@@ -0,0 +1,21 @@
+
+package Entidades;
+
+
+/**
+ *
+ * @author bustos Guada
+ */
+
+public class Pelicula {
+    
+    private String titulo;
+    private String director;
+    private String actores;
+    private String origen;
+    private String genero;
+    private Date estreno;
+    private boolean enCartelera;
+    
+    
+}
